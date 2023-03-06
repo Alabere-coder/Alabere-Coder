d4fb71694b097b6d7d40f6a05b16197c01b759a2
Alabere-coder <alabere4real247@gmail.com>
2023-03-06 16:11:21 +0100

    initial commits

---
+++ server.js
@@ -0,0 +1,78 @@
+var typed = new Typed('#multiple-text', {
+    strings: ["Web Developer", "CODER"],
+    typeSpeed: 40,
+    backSpeed: 0,
+    backDelay: 1000,
+    loop: true
+});
+
+
+
+
+
+
+
+
+// window.
+//     onload
+//     = function () {
+//         Particles.
+//             init
+//             ({
+//                 selector:
+//                     '.background'
+//             });
+//     };
+
+// Particles.
+//     init
+//     ({
+//         // normal options
+//         selector:
+//             '.background'
+//         ,
+//         maxParticles:
+//             450
+//         ,
+
+        // options for breakpoints
+    //     responsive: [
+    //         {
+    //             breakpoint:
+    //                 768
+    //             ,
+    //             options: {
+    //                 maxParticles:
+    //                     200
+    //                 ,
+    //                 color:
+    //                     '#48F2E3'
+    //                 ,
+    //                 connectParticles:
+    //                     false
+    //             }
+    //         }, {
+    //             breakpoint:
+    //                 425
+    //             ,
+    //             options: {
+    //                 maxParticles:
+    //                     100
+    //                 ,
+    //                 connectParticles:
+    //                     true
+    //             }
+    //         }, {
+    //             breakpoint:
+    //                 320
+    //             ,
+    //             options: {
+    //                 maxParticles:
+    //                     0
+
+    //                 disables particles.js
+    //             }
+    //         }
+    //     ]
+    // });
+
